cd0b5b51267fb6252321207a77b26b821265f157
Prabal Verma <prableverma@gmail.com>
2024-07-09 19:52:18 +0530

    Clerk Config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ConvexClientProvider>
-          <Header/>
+          <Header />
           {children}
         </ConvexClientProvider>
       </body>
